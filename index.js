f3386c3d434f5faebe0e3f7a316b6ba310e9e7f5
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-11-03 21:36:35 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+function introduction(name){
+    return(`Hi, my name is ${name}.`);
+}
+console.log(introduction("Josh"));
+console.log(introduction("Nancy"));
+
+function introductionWithLanguage(name, language){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+console.log(introductionWithLanguage("Josh", "Ember.js"));
+console.log(introductionWithLanguage("Nancy", "React"));
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;{
+if (language === "Python"){
+        return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    }}
+}
+console.log(introductionWithLanguageOptional("Gracie", "JavaScript"));
+console.log(introductionWithLanguageOptional("Gracie", "Python"));
